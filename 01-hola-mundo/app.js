71e26de47e6e496be5144dc49904b10b36684ee9
Victor Juan <vj.jimenez96@gmail.com>
2018-07-31 15:01:22 -0500

    Seccion 2 termnada

---
+++ 01-hola-mundo/app.js
@@ -0,0 +1,4 @@
+let nombre = 'hola Cinthya!';
+//console.log('Hola '+nombre);
+
+console.log(`Hola ${nombre}`);
---
+++ 01-hola-mundo/app3.js
@@ -0,0 +1,17 @@
+console.log('Inicio del programa');
+setTimeout(function() {
+
+    console.log('Primer TimeOUT');
+}, 3000)
+
+setTimeout(function() {
+
+    console.log('Segundo TimeOUT');
+}, 0)
+
+setTimeout(function() {
+
+    console.log('Tercer TimeOUT');
+}, 0)
+
+console.log('Fin del programa');
